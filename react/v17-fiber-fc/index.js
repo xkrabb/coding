b3fa1f29be19657520f16fa521d90439120f67b1
XU KANG <xukang@itiger.com>
2021-09-16 21:37:11 +0800

    minReact: fiber workLoop with requestIdleCallback

--- react/v17-fiber-fc/index.js
+++ react/v17-fiber-fc/index.js
@@ -20,13 +20,12 @@ function createTextElement(text) {
         }
     };
 }
-function render(element, container) {
-    const { type, props } = element;
-
+// 创建dom
+function createDom(fiber) {
+    const { type, props } = fiber;
     const dom = type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(type);
-
     const isAttr = (key) => key !== 'children' && key !== 'style';
-    const isEvent = (key) => key.startsWith('on');
+    // const isEvent = (key) => key.startsWith('on');
 
     Object.keys(props)
         .filter(isAttr)
@@ -34,10 +33,75 @@ function render(element, container) {
             dom[key] = props[key];
         });
 
-    props.children.forEach((child) => {
-        render(child, dom);
-    });
-    container.appendChild(dom);
+    return dom;
+}
+function render(element, container) {
+    nextUnitOfWork = {
+        dom: container, // stateNode
+        props: {
+            children: [element]
+        }
+    };
+}
+
+let nextUnitOfWork = null;
+
+function workLoop(deadline) {
+    let shouldYield = false;
+    while (nextUnitOfWork && !shouldYield) {
+        nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+        shouldYield = deadline.timeRemaining() < 1;
+    }
+    // 下次空闲执行
+    requestIdleCallback(workLoop);
+}
+// 空闲时候启动
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(fiber) {
+    // add dom node
+    // crate children new fiber
+    // return next unit of work
+    if (!fiber.dom) {
+        fiber.dom = createDom(fiber);
+    }
+    if (fiber.parent) {
+        fiber.parent.dom.appendChild(fiber.dom);
+    }
+
+    const elements = fiber.props.children || [];
+    let index = 0;
+    let prevSibling = null;
+    while (index < elements.length) {
+        const element = elements[index];
+        const newFiber = {
+            type: element.type,
+            props: element.props,
+            parent: fiber,
+            dom: null
+        };
+        // 递 - 构建
+        if (index === 0) {
+            // !fiber.child
+            fiber.child = newFiber;
+        } else {
+            prevSibling.sibling = newFiber;
+        }
+        prevSibling = newFiber;
+        index++;
+    }
+
+    // 归 - 返回
+    if (fiber.child) {
+        return fiber.child;
+    }
+    let nextFiber = fiber;
+    while (nextFiber) {
+        if (nextFiber.sibling) {
+            return nextFiber.sibling;
+        }
+        nextFiber = nextFiber.parent;
+    }
 }
 
 const Didact = {
